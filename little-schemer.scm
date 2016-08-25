946fb9b73227600c0e217d872005cde2ce181762
Ryan Jones <ryan.joshua.jones@gmail.com>
2016-08-25 13:50:13 -0700

    numbers

--- little-schemer.scm
+++ little-schemer.scm
@@ -17,12 +17,12 @@
 	       (member? a (cdr lat)))))))
 
 (define rember
-  (lambda (a lat)
+  (lambda (s l)
     (cond
-     ((null? lat) '())
-     ((eq? (car lat) a) (cdr lat))
-     (else (cons (car lat)
-		 (rember a (cdr lat)))))))
+     ((null? l) '())
+     ((equal? (car l) s) (cdr l))
+     (else
+      (cons (car l) (rember s (cdr l)))))))
 
 (define firsts
   (lambda (l)
@@ -271,3 +271,105 @@
      (else
       (cons (rember* a (car l)) (rember* a (cdr l)))))))
 
+(define insertR*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+	(cons old (cons new (insertR* new old (cdr l)))))
+       (else
+	(cons (car l) (insertR* new old (cdr l))))))
+     (else
+      (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
+
+(define subst*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old) (cons new (subst* new old (cdr l))))
+       (else
+	(cons (car l) (subst* new old (cdr l))))))
+     (else
+      (cons (subst* new old (car l)) (subst* new old (cdr l)))))))
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+	(cons new (cons old (insertL* new old (cdr l)))))
+       (else
+	(cons (car l) (insertL* new old (cdr l))))))
+     (else
+      (cons (insertL* new old (car l)) (insertL* new old (cdr l)))))))
+
+(define member*
+  (lambda (a l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (or
+       (eq? (car l) a)
+       (member* a (cdr l))))
+     (else
+      (or (member* a (car l)) (member* a (cdr l)))))))
+
+(define leftmost
+  (lambda (l)
+    (cond
+     ((atom? (car l)) (car l))
+    (else (leftmost (car l))))))
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1) (null? l2)) #t)
+     ((or (null? l1) (null? l2)) #f)
+     (else
+      (and
+       (equal? (car l1) (car l2))
+       (eqlist? (cdr l1) (cdr l2)))))))
+
+(define equal?
+  (lambda (s1 s2)
+    (cond
+     ((and (atom? s1) (atom? s2)) (egan? s1 s2))
+     ((or (atom? s1) (atom? s2)) #f)
+     (else (eqlist? s1 s2)))))
+
+(define numbered?
+  (lambda (aexp)
+    (cond
+     ((atom? aexp) (number? aexp))
+     (else
+      (and
+       (numbered? (car aexp))
+       (numbered? (car (cdr (cdr aexp)))))))))
+
+(define o^
+  (lambda (x y)
+    (cond
+     ((zero? y) 1)
+     (else
+      (* x (o^ x (sub1 y)))))))
+
+(define 1st-sub-exp
+  (lambda (aexp)
+    (car (cdr aexp))))
+
+(define value
+  (lambda (nexp)
+    (cond
+     ((atom? nexp) nexp)
+     ((eq? (car nexp) '+)
+      (o+ (value (cdr nexp)) (value (cdr (cdr nexp)))))
+     ((eq? (car nexp) 'x)
+      (x (value (cdr nexp)) (value (cdr (cdr nexp)))))
+     (else
+      (o^ (value (cdr nexp)) (value (cdr (cdr nexp))))))))
